0883793586d1eb7d07fc40bec6b569f7485e3b75
Dominic Musumeci <DominicMusumeci2022@gmail.com>
2023-08-13 03:59:58 -0400

    Add model

---
+++ model.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from preprocessing import image_size
+from preprocessing import data_augmentation
+from preprocessing import keras_model
+
+
+def make_model(input_shape, num_classes):
+    # preprocessing
+    keras = keras_model
+    inputs = keras.Input(shape=input_shape)
+    x = data_augmentation(inputs)
+    x = layers.Rescaling(1.0/255)(x)
+
+    # Entry block
+    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # residual
+
+    for size in [256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual]) # Add back residual
+        previous_block_activation = x # set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+    
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True, to_file="model_flowchart.png")
